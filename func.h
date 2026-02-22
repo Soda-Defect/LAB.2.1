cece7df03d63aaf306d5eb46959d9d776307420f
Arseniy <Arseniystanovskoy@mail.ru>
2026-02-22 20:16:08 +0300

    Добавление консольного меню

---
+++ func.h
@@ -0,0 +1,9 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+void print_type();
+void print_menu();
+void process(int choice, int type);
+
+
+#endif //FUNC_H
--- main.c
+++ main.c
@@ -1,5 +1,14 @@
-main()
-{
-    print("Hello world");
-    return 0;
+#include <stdio.h>
+#include "func.h"
+
+
+int main(void) {
+    int choice_type, choice;
+    do {
+        print_type();
+        scanf("%d", &choice_type);
+        print_menu();
+        scanf("%d", &choice);
+        process(choice, choice_type);
+    } while (choice != 0 && choice_type != 0);
 }
