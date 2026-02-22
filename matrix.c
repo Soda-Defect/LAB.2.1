a075e48019a9161696e9d74f95eb454eea9c0933
Arseniy <Arseniystanovskoy@mail.ru>
2026-02-22 20:39:14 +0300

    Добавление функции создания матрицы

--- matrix.c
+++ matrix.c
@@ -1,15 +1,53 @@
 #include "matrix.h"
 
-Matrix* matrix_create(int rows, int cols)
+Matrix* matrix_create(int rows, int cols, int type)
 {
+    if (rows <= 0 || cols <= 0){
+        printf("ОШИБКА: Размеры матрицы должны быть положительными!\n");
+        return NULL;
+    }
+
     Matrix* matrix = (Matrix *)malloc(sizeof(Matrix));
 
-    if(matrix == NULL){
+    if(!matrix){
+        printf("Ошибка выделения памяти для матрицы!\n");
         return NULL;
     }
 
     matrix->rows = rows;
     matrix->cols = cols;
+    matrix->data = (MatrixElement**)malloc(rows * sizeof(MatrixElement*));
 
+    if(!matrix->data){
+        free(matrix);
+        printf("Ошибка выделения памяти для строк матрицы!\n");
+        return NULL;
+    }
+
+    for(int i = 0; i < rows; i++){
+        matrix->data[i] = (MatrixElement*)malloc(cols * sizeof(MatrixElement));
+        if(!matrix->data[i]){
+            for(int j = 0; j < i; j++){
+                free(matrix->data[j]);
+            }
+            free(matrix->data);
+            free(matrix);
+            printf("Ошибка выделения памяти для строки %d матрицы!\n", i);
+            return NULL;
+        }
+
+        for(int j = 0; j < cols; j++){
+            if(type == 1){
+                matrix->data[i][j].type = TYPE_INT;
+                matrix->data[i][j].data.int_value = 0;
+            }
+            else{
+                matrix->data[i][j].type = TYPE_DOUBLE;
+                matrix->data[i][j].data.int_value = 0.0;
+            }
+        }
+    }
+
+    return matrix;
 
 }
--- matrix.h
+++ matrix.h
@@ -3,10 +3,27 @@
 
 #include <stdlib.h>
 
+// Типы данных
+typedef enum {
+    TYPE_INT,
+    TYPE_DOUBLE
+} DataType;
+
+// Структура для хранения элемента матрицы
+typedef struct {
+    DataType type;
+    union {
+        int int_value;
+        double double_value;
+    } data;
+} MatrixElement;
+
 typedef struct{
-    void* data;
-    int rows;
-    int cols;
+    MatrixElement** data; //элементы
+    int rows; //количество строк
+    int cols; //количество столбцов
 } Matrix;
+
+Matrix* matrix_create(int rows, int cols, int type);
 
 #endif //MATRIX_H
