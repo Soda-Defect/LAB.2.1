45c7825333aaaa693625c816d0a54d356aad65cb
Arseniy <Arseniystanovskoy@mail.ru>
2026-02-27 23:34:46 +0300

    Добавлен тест для комплексных

--- src/tests.c
+++ src/tests.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include "../include/matrix/matrix.h"
+#include "../include/complex.h"
 #include "../include/matrix/matrix_int.h"
 #include "../include/matrix/matrix_float.h"
 #include "../include/matrix/matrix_complex.h"
@@ -284,6 +285,162 @@ void run_float_matrix()
     matrix_free(mat);
 
     printf("\n=== Тестирование матрицы вещественных чисел закончен ===\n");
+}
+
+void run_complex_matrix()
+{
+    printf("\n=== Тестирование матрицы комплексных чисел ===\n");
+
+    printf("Создание матрицы 3x3\n");
+    Matrix* mat = create_complex_matrix(3);
+    assert(mat != NULL);
+    ASSERT_EQ(mat->razm, 3);
+
+    printf("Добавляем элементы\n");
+    int array_re[] = {-1, 5, -7, 2, 4, 9, 8, 7, -3};
+    int array_im[] = {9, -4, 1, 5, -3, 7, 2, 8, 3};
+    int elem_re, elem_im;
+    int k = 0;
+    Complex *ptr, *ptr_2, *ptr_3, *ptr_4;
+    for(int i = 0; i < mat->razm; i++){
+        for(int j = 0; j < mat->razm; j++){
+            elem_re = array_re[k];
+            elem_im = array_im[k];
+            k++;
+            Complex c = complex_create(elem_re, elem_im);
+            push_el_matrix(mat, &c, i, j);
+        }
+    }
+    print_matrix(mat);
+    k = 0;
+    for(int i = 0; i < mat->razm; i++){
+        for(int j = 0; j < mat->razm; j++){
+            elem_re = array_re[k];
+            elem_im = array_im[k];
+            k++;
+            ptr = (Complex*)element_get(mat, i, j);
+            ASSERT_EQ(ptr->real, elem_re);
+            ASSERT_EQ(ptr->imag, elem_im);
+        }
+    }
+
+    printf("Создание второй матрицы 3x3\n");
+    Matrix* mat_2 = create_int_matrix(3);
+    assert(mat_2 != NULL);
+    ASSERT_EQ(mat_2->razm, 3);
+
+    printf("Добавляем элементы\n");
+    int array_re_2[] = {8, 1, 7, 2, 4, 3, 4, 6, 9};
+    int array_im_2[] = {-1, 2, -7, 4, 3, 8, -6, 5, 1};
+    int elem_re_2, elem_im_2;
+    k = 0;
+    for(int i = 0; i < mat_2->razm; i++){
+        for(int j = 0; j < mat_2->razm; j++){
+            elem_re_2 = array_re_2[k];
+            elem_im_2 = array_im_2[k];
+            k++;
+            Complex c = complex_create(elem_re_2, elem_im_2);
+            push_el_matrix(mat, &c, i, j);
+        }
+    }
+    print_matrix(mat_2);
+    k = 0;
+    for(int i = 0; i < mat_2->razm; i++){
+        for(int j = 0; j < mat_2->razm; j++){
+            elem_re_2 = array_re_2[k];
+            elem_im_2 = array_im_2[k];
+            k++;
+            ptr = (Complex*)element_get(mat, i, j);
+            ASSERT_EQ(ptr->real, elem_re_2);
+            ASSERT_EQ(ptr->imag, elem_im_2);
+        }
+    }
+    //a.real * b.real - a.imag * b.imag, a.real * b.imag + b.real * a.imag
+
+    printf("Умножение матриц\n");
+    Matrix* mult = matrix_mult(mat, mat_2);
+    print_matrix(mult);
+    for(int i = 0; i < mat->razm; i++){
+        for(int j = 0; j < mat->razm; j++){
+            int sum = 0;
+            int sum_2 = 0;
+            for(int k = 0; k < mat -> razm; k++){
+                ptr = (Complex*)element_get(mat, i, k);
+                ptr_2 = (Complex*)element_get(mat_2, k, j);
+                sum += ptr->real * ptr_2->real - ptr->imag * ptr_2->imag;
+                sum_2 += ptr->real * ptr_2->imag + ptr_2->real * ptr->imag;
+            }
+            ptr_3 = (Complex*)element_get(mult, i, j);
+            ASSERT_EQ(ptr_3->real, sum);
+            ASSERT_EQ(ptr_3->imag, sum_2);
+        }
+    }
+
+    printf("Сложение матриц\n");
+    Matrix* sum = matrix_add(mat, mat_2);
+    print_matrix(sum);
+    for(int i = 0; i < mat->razm; i++){
+        for(int j = 0; j < mat->razm; j++){
+            ptr = (Complex*)element_get(mat, i, j);
+            ptr_2 = (Complex*)element_get(mat_2, i, j);
+            ptr_3 = (Complex*)element_get(sum, i, j);
+            ASSERT_EQ(ptr_3->real, ptr->real + ptr_2->real);
+            ASSERT_EQ(ptr_3->imag, ptr->imag + ptr_2->imag);
+        }
+    }
+
+    matrix_free(mat_2);
+
+    printf("Транспонироание матрицы\n");
+    Matrix* transp = matrix_transp(mat);
+    print_matrix(transp);
+    for(int i = 0; i < mat->razm; i++){
+        for(int j = 0; j < mat->razm; j++){
+            ptr = (Complex*)element_get(mat, i, j);
+            ptr_2 = (Complex*)element_get(transp, j, i);
+            ASSERT_EQ(ptr->real, ptr_2->real);
+            ASSERT_EQ(ptr->imag, ptr_2->imag);
+        }
+    }
+
+    printf("Прибавление к строке 1 линейной комбинации строк, умноженных на 2 и 1\n");
+    mat_2 = create_int_matrix(3);
+    k = 0;
+    for(int i = 0; i < mat_2->razm; i++){
+        for(int j = 0; j < mat_2->razm; j++){
+            elem_re = array_re[k];
+            elem_im = array_im[k];
+            k++;
+            Complex c = complex_create(elem_re, elem_im);
+            push_el_matrix(mat_2, &c, i, j);
+        }
+    }
+    int rowIndex = 1;
+    int alpha1 = 2, alpha2 = 1;
+    matrix_add_line_comb(mat, rowIndex - 1, 1, &alpha1);
+    matrix_add_line_comb(mat, rowIndex - 1, 2, &alpha2);
+    print_matrix(mat);
+
+    ptr = (Complex*)element_get(mat, 0, 0);
+    ptr_2 = (Complex*)element_get(mat_2, 0, 0);
+    ptr_3 = (Complex*)element_get(mat_2, 1, 0);
+    ptr_4 = (Complex*)element_get(mat_2, 2, 0);
+    ASSERT_EQ(*ptr, *ptr_2 + *ptr_3 * 2 + *ptr_4);
+    ptr = (Complex*)element_get(mat, 0, 1);
+    ptr_2 = (Complex*)element_get(mat_2, 0, 1);
+    ptr_3 = (Complex*)element_get(mat_2, 1, 1);
+    ptr_4 = (Complex*)element_get(mat_2, 2, 1);
+    ASSERT_EQ(*ptr, *ptr_2 + *ptr_3 * 2 + *ptr_4);
+    ptr = (Complex*)element_get(mat, 0, 2);
+    ptr_2 = (Complex*)element_get(mat_2, 0, 2);
+    ptr_3 = (Complex*)element_get(mat_2, 1, 2);
+    ptr_4 = (Complex*)element_get(mat_2, 2, 2);
+    ASSERT_EQ(*ptr, *ptr_2 + *ptr_3 * 2 + *ptr_4);
+
+    matrix_free(mat_2);
+    matrix_free(mat);
+
+    printf("\n=== Тестирование матрицы комплексных чисел закончен ===\n");
 }
 
 void run_all_matrix()
